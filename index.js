75b5d3985549ff34be6d1801d617d631885f9193
TheGamesss <gabe.f.arreguin@gmail.com>
2025-08-04 21:39:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\GA\AppData\Local\Temp\tmp3c8t61fi.js
+// include: C:\Users\GA\AppData\Local\Temp\tmp9v1bc8ae.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -210,21 +210,21 @@ Module['FS_createPath']("/", "res", true, true);
 
   })();
 
-// end include: C:\Users\GA\AppData\Local\Temp\tmp3c8t61fi.js
-// include: C:\Users\GA\AppData\Local\Temp\tmp7p26h49n.js
+// end include: C:\Users\GA\AppData\Local\Temp\tmp9v1bc8ae.js
+// include: C:\Users\GA\AppData\Local\Temp\tmp99z7va9_.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\GA\AppData\Local\Temp\tmp7p26h49n.js
-// include: C:\Users\GA\AppData\Local\Temp\tmphszs4ofj.js
+  // end include: C:\Users\GA\AppData\Local\Temp\tmp99z7va9_.js
+// include: C:\Users\GA\AppData\Local\Temp\tmpzoslpcup.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\GA\AppData\Local\Temp\tmphszs4ofj.js
+  // end include: C:\Users\GA\AppData\Local\Temp\tmpzoslpcup.js
 
 
 var arguments_ = [];
